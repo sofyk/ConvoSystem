934a8e82ff70012747bd936c66424ebbde40d8f0
sofyk <sofy.k87@gmail.com>
2014-02-22 14:07:54 -0800

    Working out the class structure for Convoscripts

--- ConvoSystem/convolib.cpp
+++ ConvoSystem/convolib.cpp
@@ -1,1 +1,11 @@
 #include "convolib.h"
+
+ConvoScript::ConvoScript(){
+    script.clear();
+    scriptSize = 0;
+}
+
+void ConvoScript::addNode(){
+
+}
+
--- ConvoSystem/convolib.h
+++ ConvoSystem/convolib.h
@@ -1,19 +1,32 @@
 #ifndef CONVOLIB_H
 #define CONVOLIB_H
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <QObject>
+using namespace std;
 
-class ConvoScript
+class ConvoScript : public QObject
 {
-    ConvoNode start;
+    //ConvoNode start;
+    //ConvoNode script[];
+    std::vector< ConvoNode > script;
+    int scriptSize;
 public:
     ConvoScript();
-    addNode();
+    void addNode();
     delNode();
     getNode();
+    getScriptSize();
 };
 
-class ConvoNode
+class ConvoNode : public QObject
 {
     int nodeId;
+    int npcFieldNum;
 public:
     ConvoNode();
     delConvoNode();
@@ -23,6 +36,36 @@ public:
     delNpcField();
     addPlayerField();
     delPlayerField();
+};
+
+class NpcField : public QObject
+{
+    TextField textField;
+public:
+    setTextField();
+    getTextField();
+};
+
+class PlayerField : public QObject
+{
+    TextField textField;
+    ConvoNode nextNode;
+public:
+    setTextField();
+    getTextField();
+    setNextNode();
+    getNextNode();
+};
+
+class TextField : public QObject
+{
+    string fieldId;
+    string fieldText;
+public:
+    getFieldId();
+    setFieldId();
+    getFieldText();
+    setFieldText();
 };
 
 #endif // CONVOLIB_H
